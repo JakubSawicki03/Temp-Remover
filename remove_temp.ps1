9c4fe05f2abd0e3cbbdb383d6ff50f265a07816a
Jakub Sawicki <kubasaw03@gmail.com>
2023-08-18 07:01:38 +0200

    first commit

---
+++ remove_temp.ps1
@@ -0,0 +1,13 @@
+﻿# REMOVING TEMP IN %APPDATA%
+
+Remove-Item -Path "C:\Users\jsawicki\AppData\Local\Temp" -Force -Recurse -ErrorAction SilentlyContinue
+
+# REMOVING TEMP IN WINDOWS
+
+Remove-Item -Path "C:\Windows\Temp" -Force -Recurse -ErrorAction SilentlyContinue
+
+# CLEANING RECYCLE BIN
+
+Remove-Item -Path $driveName\`$recycle.bin -Recurse -Force -ErrorAction SilentlyContinue
+
+Set-ExecutionPolicy Default
